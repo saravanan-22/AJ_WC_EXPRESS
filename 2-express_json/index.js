b51b84c7e95f6dc55df442d7aa9b25ee574348a7
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-08-19 18:16:27 +0530

    inicial_commit

---
+++ 2-express_json/index.js
@@ -0,0 +1,64 @@
+import express from "express";
+import { products, users } from "./data.js";
+
+const PORT = 5000;
+const app = express();
+
+//http://localhose:5000  Base URL
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>This is a homepage</h1>");
+});
+
+// http://localhost:5000/api/products
+
+// app.get("/api/products" ,(req, res) => {
+//     res.status(200).json({message: "success", data : products});
+// });
+//
+// Minimum details
+app.get("/api/products", (req, res) => {
+  let newProducts = products.map(({id, title, price, category, image}) => {
+    return { id, title,price, category, image };
+  });
+  res.status(200).json(newProducts);
+});
+
+// http://localhost:5000/api/products-details
+
+app.get("/api/products-details" ,(req, res) => {
+    res.status(200).json({message: "success", data : products});
+});
+
+// Minimum details
+
+//http://localhost:5000/api/users
+
+app.get("/api/users", (req, res) => {
+  res.status(200).json(users);
+});
+
+
+// params  http://localhost:5000/api/products/:id
+
+app.get("/api/products/:id",(req, res)=> {
+    // console.log(req.params);             // to identify the id
+    const {id} = req.params
+    let singleProducts = products.find((product) => product.id === Number(id));
+    if(!singleProducts){
+        res.status(400).send(`<h1>Product not found this id:${id}</h1> 
+        <a href="/api/products-details">Click here to go for all product-detail page`)
+    }
+    res.status(200).json(singleProducts);
+})
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`<h1>This server is running in: http://localhost:${PORT}`);
+});
